adcb71de58b5084c75f0853bc64098c147208c69
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2019-05-09 10:48:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const defaultParam = {
+  query: '',
+  value: '',
+};
 
 function App() {
+  const [baseUrl, setBaseUrl] = React.useState('');
+
+  const handleBaseUrlChange = event => {
+    const { value } = event.target;
+    setBaseUrl(value);
+  };
+
+  const [params, setParams] = React.useState([]);
+
+  const addParam = (param = defaultParam) => {
+    setParams(oldParams => [...oldParams, param]);
+  };
+
+  const handleQueryRemove = index => {
+    setParams(oldParams => oldParams.filter((param, i) => i !== index));
+  };
+
+  const handleQueryChange = (index, key, value) => {
+    setParams(oldParams =>
+      oldParams.map((param, i) => {
+        if (i !== index) return param;
+        return {
+          ...param,
+          [key]: value,
+        };
+      })
+    );
+  };
+
+  React.useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('base_url')) {
+      setBaseUrl(urlParams.get('base_url'));
+    }
+
+    if (urlParams.has('query')) {
+      const queries = urlParams.getAll('query');
+      queries.forEach(query => {
+        addParam({ ...defaultParam, query });
+      });
+    }
+  }, []);
+
+  const queryString = params
+    .map(
+      ({ query, value }) =>
+        `${encodeURIComponent(query)}=${encodeURIComponent(value)}`
+    )
+    .join('&');
+
+  const url = baseUrl && `${baseUrl}${queryString && `?${queryString}`}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Base URL:{' '}
+      <input
+        value={baseUrl}
+        onChange={handleBaseUrlChange}
+        placeholder="https://..."
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Query</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {params.map(({ query, value }, i) => (
+            <tr key={i}>
+              <th>
+                <input
+                  value={query}
+                  onChange={({ target }) =>
+                    handleQueryChange(i, 'query', target.value)
+                  }
+                />
+              </th>
+              <th>
+                <input
+                  value={value}
+                  onChange={({ target }) =>
+                    handleQueryChange(i, 'value', target.value)
+                  }
+                />
+              </th>
+              <th>
+                <button onClick={() => handleQueryRemove(i)}>Remove</button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={addParam}>Add parameter</button>
+      <a href={url}>{url}</a>
     </div>
   );
 }
